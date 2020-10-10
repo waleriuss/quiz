38edba45e99ceaaa04f8050caf32ff9cf8739229
waleriuss <jakubwaliczek2002@gmail.com>
2020-10-10 18:47:45 +0100

    pytania.js

---
+++ pytania.js
@@ -0,0 +1,147 @@
+const odp =[
+	{
+		pytanie:"Jaki obraz mieści się w kościele p.w. Św. Mikołaja w Łące ?",
+		odp:[
+			{text: "Św. Mikołaja biskupa z Mirry",prawda:true},
+			{text: "Św. Dominika biskupa z Padwy",prawda:false},
+			{text: "Św. Jana biskupa z Katowic	",prawda:false},
+			{text: "Św. Marcina z Mirry",prawda:false}
+		]
+	},
+	{
+		pytanie:"Z którego wieku pochodzi kościół św.Mikołaja w Łące ?",
+		odp:[
+			{text: "Świątynia pochodzi z XX wieku",prawda:true},
+			{text: "Świątynia pochodzi z XVIII wieku",prawda:false},
+			{text: "Świątynia pochodzi z XV wieku",prawda:false},
+			{text: "Świątynia pochodzi z XVII wieku",prawda:true}
+		]
+	},
+	{
+		pytanie:"Jaką konstrukcję budowlaną posiada kościół pw.św.Marcina w Ćwiklicach ?",
+		odp:[
+			{text: "betonową",prawda:false},
+			{text: "o konstrukcji zrębowej",prawda:true},
+			{text: "o konstrukcji z cegły czerwonej",prawda:false},
+			{text: "o konstrukcji zrębowo betonowej",prawda:false}
+		]
+	},
+	{
+		pytanie:"Z jakiej epoki pochodzi budowla kościoła św.Marcina w Ćwiklicach ?",
+		odp:[
+			{text: "z przełomu XVI i XVII",prawda:true},
+			{text: "z przełomu XV i XVI",prawda:false},
+			{text: "z przełomu XI i XII",prawda:false},
+			{text: "z przełomu XVII i XVIII",prawda:false}
+		]
+	},
+	{
+		pytanie:"Jaką konstrukcję posiada wieża kościoła pw.św.Jana Chrzciciela w Grzawej ?",
+		odp:[
+			{text: "konstrukcji słupowej",prawda:true},
+			{text: "konstrukcji metalowej",prawda:false},
+			{text: "konstrukcji drewnianej",prawda:false},
+			{text: "konstrukcji wieżowej",prawda:false}
+		]
+	},
+	{
+		pytanie:"Co można ujrzeć w środku kościoła pw.św.Jana Chrzciciela w Grzawej ?",
+		odp:[
+			{text: "mała kapliczka z obrazem Matki Boskiej Pszczyńskiej",prawda:false},
+			{text: "ołtarz główny z obrazem Św. Siostry Faustyny Kowalskiej",prawda:false},
+			{text: "ołtarz boczny z obrazem Św. Jana Bosko",prawda:false},
+			{text: "ołtarz główny z obrazem głowy Św. Jana chrzciciela",prawda:true}
+		]
+	},
+	{
+		pytanie:"W jakich latach powstał kościół pw. św. Katarzyny w Pielgrzymowicach ?",
+		odp:[
+			{text: "Powstał w latach 1672-1673",prawda:false},
+			{text: "Powstał w latach 1678-1680",prawda:false},
+			{text: "Powstał w latach 1675-1680",prawda:true},
+			{text: "Powstał w latach 1660-1670obrazem olejnym przedstawiającym Trójcę Świętą",prawda:false}
+		]
+	},
+	{
+		pytanie:"Co przedstawia ołtarzyk boczny w kościele pw. św. Katarzyny w Pielgrzymowicach ?",
+		odp:[
+			{text: "obraz olejny przedstawiający Trójcę Świętą",prawda:true},
+			{text: "obraz przedstawiający św.Brygide",prawda:false},
+			{text: "obraz przedstawiający Boga Ojca",prawda:false},
+			{text: "obraz olejny przedstawiający Modlitwę Pana Jezusa w Ogrodzie Oliwnym",prawda:false}
+		]
+	},
+	{
+		pytanie:"Z którego wieku pochodzi kościół, jak również zbudowana później wieża dzwonna Kościóła pw. św. Jakuba Starszego Apostoła w Wiśle Małej ?",
+		odp:[
+			{text: "XVII",prawda:false},
+			{text: "XVIII",prawda:true},
+			{text: "XIX",prawda:false},
+			{text: "XVI",prawda:false}
+		]
+	},
+	{
+		pytanie:"Cechą charakterystyczną Kościoła pw. św. Jakuba Starszego Apostoła w Wiśle Małej jest empora, gdzie ona została umieszczona ?",
+		odp:[
+			{text: "umieszczona na wschodniej ścianie nawy",prawda:false},
+			{text: "umieszczona na zachodniej ścianie nawy",prawda:true},
+			{text: "umieszczona na północnej ścianie nawy",prawda:false},
+			{text: "umieszczona na południowej ścianie nawy",prawda:false}
+		]
+	},
+	{
+		pytanie:"Jaka jest największa drewniania świątynia w powiecie ?",
+		odp:[
+			{text: "Kościół św. Klemensa w Miedźnej",prawda:true},
+			{text: "Kościół św. Jakuba Starszego Apostoła w Wiśle MałejKościół ",prawda:false},
+			{text: "Kościół św. Jakuba Starszego Apostoła w Wiśle Małej",prawda:false},
+			{text: "Kościół św. Jana Chrzciciela w Grzawej",prawda:false}
+		]
+	},
+	{
+		pytanie:"Co znajduje się nad nawą w kościele pw. św. Klemensa w Miedźnej ?",
+		odp:[
+			{text: "Nad nawą umieszczona jest wieżyczka sygnaturki z barokową latarnią",prawda:true},
+			{text: "Nad nawą umieszczona jest figurka z barokową latarnią",prawda:false},
+			{text: "Nad nawą umieszczono obraz z barokową latarnią",prawda:false},
+			{text: "Nad nawą umieszczona jest wieżyczka z drewnianym Krzyżem",prawda:false}
+		]
+	},
+	{
+		pytanie:"Co można zobaczyć w kościele pw. św. Barbary w Górze ?",
+		odp:[
+			{text: "szklane prezbiterium",prawda:false},
+			{text: "relikwie św Jana Pawła 2",prawda:false},
+			{text: "środek kościoła w stylu gotyckim",prawda:false},
+			{text: "trójboczne prezbiterium",prawda:true}
+		]
+	},
+	{
+		pytanie:"W którym roku pierwiotnie postawiono kaplicę Kościóła pw. św. Barbary w Górze ?",
+		odp:[
+			{text: "1950",prawda:true},
+			{text: "1951",prawda:false},
+			{text: "1945",prawda:false},
+			{text: "1953",prawda:false}
+		]
+	},
+	{
+		pytanie:"W którym roku Ekspozycja Zagrody Wsi Pszczyńskiej w Pszczynie została utworzona ?",
+		odp:[
+			{text: "w 1960",prawda:false},
+			{text: "w 1974",prawda:false},
+			{text: "w 1975",prawda:true},
+			{text: "w 1980",prawda:false}
+		]
+	},
+	{
+		pytanie:"Czy w Skansenie Zagrody Wsi Pszczyńskiej znajduje się ?",
+		odp:[
+			{text: "zabytkowy młyn",prawda:true},
+			{text: "zabytkowa drukarnia prasy śląskiej",prawda:false},
+			{text: "zabytkowy drewniany Kościółek",prawda:false},
+			{text: "Zagroda żubrów",prawda:false}
+		]
+	}
+]
+
